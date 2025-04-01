d5fa39129962ea76931cc1183ccf79abe4af74f5
masakinihirota <masakinihirota@gmail.com>
2025-04-01 20:11:12 +0900

    TypeScript設定を更新し、Reactのバージョンを19にアップグレード。フォームコンポーネントでuseFormStateをuseActionStateに変更。

--- components/ForgotPasswordForm.tsx
+++ components/ForgotPasswordForm.tsx
@@ -1,15 +1,15 @@
 
+import { useActionState } from "react";
 "use client"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useFormState } from 'react-dom'
 import { forgotPassword } from '@/app/auth/actions'
 export default function ForgotPasswordForm() {
     const initialState = {
         message: ''
     }
-    const [formState, formAction] = useFormState(forgotPassword, initialState)
+    const [formState, formAction] = useActionState(forgotPassword, initialState)
     return (<>
         <form action={formAction}>
             <div className="grid gap-2">
